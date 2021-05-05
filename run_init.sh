feb78ab33962365714e89182bf57aa5b10133fcd
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2021-05-06 03:15:47 +0900

    Add files via upload

--- run_init.sh
+++ run_init.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 echo "안녕하세요."
-printf "현재시각은 "
 #!/bin/sh
-timestamp=`date +%Y-%m-%d\(%H:%M\)`
-printf "$timestamp"
-echo " 입니다."
-echo "현재폴더의 파일은 다음과 같습니다."
-ls
-echo ""
-
 alias e='explorer.exe .'
 alias shutdown='sudo shutdown -h now'
 alias reboot='sudo shutdown -r now'
